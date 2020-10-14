c5960f70f6956d29dd37b5148d59cc640560bd9e
patkub2 <patrykkubala1999@gmail.com>
2020-10-14 21:25:20 +0200

    added gitignore // added homework

--- test.cpp
+++ test.cpp
@@ -5,14 +5,15 @@ using namespace std;
 int main()
 {
     int totalmoney;
-    cout << "please put number of times"  << endl;
+    cout << "please put number of times" << endl;
     cin >> totalmoney;
-int banknotes = 0;
-    while(totalmoney >= 50){
-         totalmoney = totalmoney - 50;
-         banknotes = banknotes +1;
+    int banknotes = 0;
+    while (totalmoney >= 50)
+    {
+        totalmoney = totalmoney - 50;
+        banknotes = banknotes + 1;
     }
 
-    cout << "number of banknotes: "<< banknotes  << endl;
+    cout << "number of banknotes: " << banknotes << endl;
     return 0;
 }
