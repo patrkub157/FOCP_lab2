75090e76cc45fdf1607bc45af41974d2c70e40dc
patkub2 <patrykkubala1999@gmail.com>
2020-10-15 12:24:52 +0200

    adding function

--- main.cpp
+++ main.cpp
@@ -2,11 +2,34 @@
 
 using namespace std;
 
+bool is_prime(int number)
+{
+
+    for (int i = 2; i < number / 2; i++)
+    {
+        if (number % i == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 int main()
 {
+    cout << "Please put a number:" << endl;
+
     int a;
-    //<< endl
-    cout << "Please put in a number: ";
     cin >> a;
+
+    if (is_prime(a))
+    {
+        std::cout << "Your number is prime" << std::endl;
+    }
+    else
+    {
+        std::cout << "Your number is NOT prime" << std::endl;
+    }
     return 0;
 }
