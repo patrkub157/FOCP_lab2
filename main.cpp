1df6539215bb30dbf6cb759726696391760d7a72
patkub2 <patrykkubala1999@gmail.com>
2020-10-15 13:32:30 +0200

    fibonacii program

--- main.cpp
+++ main.cpp
@@ -2,36 +2,57 @@
 
 using namespace std;
 
-int pow(int base, int power)
+int fibonacci_iterative(int index)
 {
-    int result = 1;
+    int a = 0;
+    int b = 1;
 
-    for (int i = 0; i < power; i++)
+    if (index < 2)
     {
-        result *= base;
+        return index;
     }
 
-    return result;
+    for (int i = 1; i < index; i++)
+    {
+        int tmp = a + b;
+        a = b;
+        b = tmp;
+    }
+
+    return b;
+}
+
+int fibonacci_recursive(int index)
+{
+    if (index < 2)
+    {
+        return index;
+    }
+
+    return fibonacci_recursive(index - 2) + fibonacci_recursive(index - 1);
 }
 
 int read_int()
 {
-    cout << "Please type a number:" << endl;
+    cout << "Put a number:" << endl;
 
-    int n;
-    cin >> n;
+    int k;
+    cin >> k;
 
-    return n;
+    return k;
 }
 
 int main()
 {
-    int base = read_int();
-    int power = read_int();
+    int index = read_int();
 
-    int result = pow(base, power);
+    int result_iterative = fibonacci_iterative(index);
 
-    cout << "Result: " << result << endl;
+    cout << "Result with iteration is: " << result_iterative << endl;
+
+    int result_recursive = fibonacci_recursive(index);
+
+    cout << "Result with recursion is: " << result_recursive << endl;
 
     return 0;
 }
